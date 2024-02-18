00eb97c53a0d380d58111de6119b3b1905da6d2e
heiisenber6 <greatiftekher@gmail.com>
2024-02-19 00:54:09 +0600

    asd

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin : process.env.FRONTEND_URL,
+  origin : 'https://final-todo-umber.vercel.app/',
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true,
 }));
